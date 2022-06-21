c4fc5f5cc8bfdaa9acceb7d3b5d79a3db931bbfd
ItaloCobains <monokatchaleger@gmail.com>
2022-06-21 14:20:14 -0300

    adicionando button ao app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
+import LinkButton from './components/LinkButton';
 
 const App = () => {
 
   return (
     <div>
-      ...
+      <LinkButton
+        name="Instagram"
+        link="https://www.instagram.com/devpobrerico/"
+      />
+      <LinkButton
+        name="GitHub"
+        link="https://github.com/gotham-ong"
+      />
+      <LinkButton
+        name="Discord"
+        link="https://discord.gg/gfuDsPbxGj"
+      />
+      <LinkButton
+        name="LinkedIn"
+        link="https://www.linkedin.com/company/gotham-ong/"
+      />
+
     </div>
   )
 }
 
 
-export default App
+export default App;
